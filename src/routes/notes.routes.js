3b77b5fcd92787c82b2187b73fd525ad31a53b71
AndressaR <dre.rodrigues27@gmail.com>
2023-03-01 19:58:10 -0300

    Adding middleware autentication

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 const NotesController = require("../controllers/NotesController");
+const ensureAuthenticated = require("../Middleware/ensureAuthenticated")
 
 const notesRoutes = Router();
 
 //Como temos uma classe dentro do UsersController é preciso criar uma nova instância para essa classe. 
 const notesController = new NotesController();
 
+notesRoutes.use(ensureAuthenticated);
 
-notesRoutes.post("/:user_id", notesController.create); // id do usuário
+
+notesRoutes.post("/", notesController.create); // id do usuário
 notesRoutes.get("/:id", notesController.show); //id da nota
 notesRoutes.delete("/:id", notesController.delete);
 notesRoutes.get("/", notesController.index);
--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const TagsController = require("../controllers/TagsController");
+const ensureAuthenticated = require("../Middleware/ensureAuthenticated")
 
 const tagsRoutes = Router();
 
 //Como temos uma classe dentro do UsersController é preciso criar uma nova instância para essa classe. 
 const tagsController = new TagsController();
 
-tagsRoutes.get("/:user_id", tagsController.index);
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
 
  module.exports = tagsRoutes;
